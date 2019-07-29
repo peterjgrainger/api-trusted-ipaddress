6d8953146e0c90788e7ec729beb577f5f49f4f30
Peter Grainger <peter@grainger.xyz>
2019-07-29 20:49:41 +0100

    Added some comments

--- src/middleware/redis.js
+++ src/middleware/redis.js
@@ -1,12 +1,16 @@
 const redis = require('redis');
 
-// reusable client
+// Reusable client
 let client;
+
 const clientConfiguration = {
   port: '6379',
   host: process.env.REDIS_HOST || 'localhost'
 };
 
+/**
+ * Attach the redis client to the request to be used by the routes
+ */
 function redisMiddleware(req, res, next) {
   client = client || redis.createClient(clientConfiguration);
   req.redisClient = client;
--- src/routes/trusted-ipaddress.js
+++ src/routes/trusted-ipaddress.js
@@ -1,7 +1,12 @@
+// JSON message to send if the IP address is not found in redis
 const trustedMessage = {
   trusted: true
 };
 
+/**
+ * JSON message to send if the IP address is found in redis
+ * @param {string} source ip address source list
+ */
 function untrustedMessage(source) {
   return {
     trusted: false,
@@ -9,6 +14,9 @@ function untrustedMessage(source) {
   };
 }
 
+/**
+ * Look in redis for the IP address passed in.
+ */
 function trustedIpAddressRoute(req, res) {
   req.redisClient.get(req.params.ipAddress, (error, source) => {
     const message = source ? untrustedMessage(source) : trustedMessage;
